6af5bf7ffec3a19ced669b6081b422e11ae90e2c
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-07 15:54:17 +0300

    add services and host

--- OnChat.Server/Program.cs
+++ OnChat.Server/Program.cs
@@ -1,30 +1,39 @@
-﻿using OnChat;
-using OnChat.Protocol.Codecs;
-using OnChat.Shared;
+﻿using DataModel;
+using LinqToDB;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OnChat;
+using OnChat.Connection;
+using OnChat.Encryption;
+using OnChat.Shared.Messages;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration().WriteTo.Console(
     outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}"
 ).CreateLogger();
 
-Server server = new();
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Services.AddSingleton<ConnectionsList>();
+builder.Services.AddSerilog();
+builder.Services.AddSingleton<JwtTokensService>();
+builder.Services.AddSingleton(sp => new Server(
+        sp.GetService<ConnectionsList>()!,
+        sp,
+        typeof(SendMessagePacket).Assembly
+    )
+);
 
-MessagePacket packet = new()
-                       {
-                           UserId = "druzhe",
-                           Message = "hello"
-                       };
+builder.Services.AddScoped(provider =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Default");
+    var options = new DataOptions<OnChatDb>(new DataOptions().UsePostgreSQL(connectionString));
+    // var options = provider.GetRequiredService<DataOptions<OnChatDb>>();
+    return new OnChatDb(options);
+});
 
-MemoryStream ms = new();
-BinaryWriter msW = new(ms);
-BinaryReader msR = new(ms);
+IHost app = builder.Build();
+await app.StartAsync();
 
-ICodec codec = server.Protocol.GetCodec(typeof(MessagePacket));
-codec.Encode(msW, packet);
-ms.Seek(0, SeekOrigin.Begin);
-
-MessagePacket packetRead = (MessagePacket) codec.Decode(msR);
-
-Log.Information($"{packetRead.UserId} {packetRead.Message}");
-
-//await server.Start();
+await app.Services.GetService<Server>()!.Start();
