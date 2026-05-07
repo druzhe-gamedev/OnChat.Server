f385d17bf5f2958cd721a65f4de13d54cd520d66
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-07 15:53:20 +0300

    add auth handlers

---
+++ OnChat.Server/UsersManagement/Authentication/AuthenticationHandler.cs
@@ -0,0 +1,39 @@
+﻿using DataModel;
+using LinqToDB.Async;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OnChat.Configuration;
+using OnChat.Encryption;
+using OnChat.Protocol.PacketHandler;
+using OnChat.Shared.Auth;
+
+namespace OnChat.UsersManagement.Authentication;
+
+public class AuthenticationHandler(
+    OnChatDb db,
+    JwtTokensService tokensService,
+    IConfiguration configuration,
+    ILogger<AuthenticationHandler> logger
+) : PacketHandler<AuthenticationPacket>
+{
+    protected override async Task Handle(AuthenticationPacket packet, IConnection caller)
+    {
+        logger.LogInformation("Logging in");
+        
+        User? user = await db.Users.FirstOrDefaultAsync(user =>
+            (user.Mail == packet.Login || user.Username == packet.Login) && 
+            user.PasswordHash == HashHelper.GetHash(packet.Password, configuration[ConfigurationConstants.HashSalt]!)
+        );
+        
+        if(user is not null)
+        { 
+            TokenModel jwtToken = tokensService.CreateJwtToken(("id", user.Id), ("username", user.Username)!);
+            TokenModel refreshToken = tokensService.CreateRefreshToken();
+            await caller.Write(new TokensPacket(packet.CorrelationId, jwtToken, refreshToken));
+            return;
+        }
+        
+        // todo failure packet
+        // caller.Write(new AuthenticationFailurePacket());
+    }
+}
---
+++ OnChat.Server/UsersManagement/Registration/RegistrationHandler.cs
@@ -0,0 +1,38 @@
+﻿using DataModel;
+using LinqToDB;
+using LinqToDB.Async;
+using Microsoft.Extensions.Configuration;
+using OnChat.Configuration;
+using OnChat.Encryption;
+using OnChat.Protocol.PacketHandler;
+using OnChat.Shared.Auth;
+
+namespace OnChat.UsersManagement.Registration;
+
+public class RegistrationHandler(OnChatDb db, IConfiguration configuration/*, JwtTokensService tokensService*/) : PacketHandler<RegistrationPacket>
+{
+    protected override async Task Handle(RegistrationPacket packet, IConnection caller)
+    {
+        User? user =
+            await db.Users.FirstOrDefaultAsync(user => user.Username == packet.Username || user.Mail == packet.Email);
+
+        if (user is null)
+        {
+            RegistrationSuccessfulResponse response = new(packet.CorrelationId, "Success");
+
+            string passwordHash = HashHelper.GetHash(packet.Password, configuration[ConfigurationConstants.HashSalt]);
+            await db.InsertAsync(new User
+                                 {
+                                     Username = packet.Username,
+                                     Mail = packet.Email,
+                                     Age = packet.Age,
+                                     PasswordHash = passwordHash
+                                 });
+            
+            await caller.Write(response);
+            return;
+        }
+
+        await caller.Write(new RegistrationFailureResponse(packet.CorrelationId, "User already exists"));
+    }
+}
