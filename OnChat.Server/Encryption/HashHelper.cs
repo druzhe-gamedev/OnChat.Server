fc7352fc9b25ccebd83588b18b3770f996342e44
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-07 15:53:39 +0300

    add HashHelper

---
+++ OnChat.Server/Encryption/HashHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace OnChat.Encryption;
+
+public static class HashHelper
+{
+    private static readonly SHA256 Sha256 = SHA256.Create();
+
+    public static string GetHash(string str, string? salt = "") =>
+        BytesToString(Sha256.ComputeHash(Encoding.UTF8.GetBytes(str + salt)));
+
+    private static string BytesToString(byte[] hashBytes)
+    {
+        StringBuilder hash = new();
+
+        foreach (byte hashByte in hashBytes)
+            hash.Append(hashByte.ToString("x2"));
+
+        return hash.ToString();
+    }
+}
