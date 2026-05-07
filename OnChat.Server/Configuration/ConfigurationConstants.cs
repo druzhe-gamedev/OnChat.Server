f5883b668a15cd324e667da5b1a096f81b46b8df
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-07 15:52:40 +0300

    add ConfigurationConstants

---
+++ OnChat.Server/Configuration/ConfigurationConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace OnChat.Configuration;
+
+public static class ConfigurationConstants
+{
+    public static readonly string HashSalt = "HashSalt";
+    public static readonly string Secret = "Secret";
+}
