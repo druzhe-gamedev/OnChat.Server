1ac98c10bf9cfd85e228c71a038736a38b4940a1
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 00:33:07 +0300

    add TokensRotationHandler

---
+++ OnChat.Server/UsersManagement/Tokens/TokensRotationHandler.cs
@@ -0,0 +1,28 @@
+﻿using OnChat.Encryption;
+using OnChat.Protocol.PacketHandler;
+using OnChat.Shared.Auth;
+
+namespace OnChat.UsersManagement.Tokens;
+
+public class TokensRotationHandler(TokensService tokensService) : PacketHandler<TokensRotationPacket>
+{
+    protected override async Task Handle(TokensRotationPacket packet, IConnection caller)
+    {
+        if (caller.AuthenticationState is not Authenticated authenticated)
+        {
+            await caller.Write(new UnauthorizedPacket(packet.CorrelationId, "Unauthorized"));
+            return;
+        }
+
+        if (!tokensService.IsRefreshTokenExist(authenticated.UserId))
+        {
+            await caller.Write(new UnauthorizedPacket(packet.CorrelationId, "Refresh token has been expired"));
+            return;
+        }
+
+        TokenModel jwtToken = tokensService.CreateJwtToken(("id", authenticated.UserId), ("username", authenticated.Username));
+        TokenModel refreshToken = tokensService.CreateRefreshToken();
+        tokensService.SaveRefreshToken(authenticated.UserId, refreshToken.Token);
+        await caller.Write(new TokensPacket(packet.CorrelationId, jwtToken, refreshToken));
+    }
+}
