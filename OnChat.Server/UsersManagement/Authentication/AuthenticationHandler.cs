0276673020f703d394cda315cc6298dafbc7652e
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 00:34:24 +0300

    add failure cases, inject connections provider

--- OnChat.Server/UsersManagement/Authentication/AuthenticationHandler.cs
+++ OnChat.Server/UsersManagement/Authentication/AuthenticationHandler.cs
@@ -3,6 +3,7 @@ using LinqToDB.Async;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using OnChat.Configuration;
+using OnChat.Connection;
 using OnChat.Encryption;
 using OnChat.Protocol.PacketHandler;
 using OnChat.Shared.Auth;
@@ -11,7 +12,8 @@ namespace OnChat.UsersManagement.Authentication;
 
 public class AuthenticationHandler(
     OnChatDb db,
-    JwtTokensService tokensService,
+    TokensService tokensService,
+    ConnectionsProvider connectionsProvider,
     IConfiguration configuration,
     ILogger<AuthenticationHandler> logger
 ) : PacketHandler<AuthenticationPacket>
@@ -20,20 +22,31 @@ public class AuthenticationHandler(
     {
         logger.LogInformation("Logging in");
         
-        User? user = await db.Users.FirstOrDefaultAsync(user =>
-            (user.Mail == packet.Login || user.Username == packet.Login) && 
-            user.PasswordHash == HashHelper.GetHash(packet.Password, configuration[ConfigurationConstants.HashSalt]!)
-        );
-        
+        User? user = await db.Users.FirstOrDefaultAsync(user => user.Mail == packet.Login || user.Username == packet.Login);
+        // todo add validations
         if(user is not null)
-        { 
+        {
+            if (user.PasswordHash != HashHelper.GetHash(
+                    packet.Password,
+                    configuration[ConfigurationConstants.HashSalt]!
+                ))
+            {
+                await caller.Write(new WrongPasswordPacket(packet.CorrelationId, "Wrong password"));
+                return;
+            }
+            
             TokenModel jwtToken = tokensService.CreateJwtToken(("id", user.Id), ("username", user.Username)!);
             TokenModel refreshToken = tokensService.CreateRefreshToken();
+            tokensService.SaveRefreshToken(user.Id, refreshToken.Token);
+            caller.Authenticate(user.Id, user.Username!);
+
+            // todo check auth
+            Authenticated? authenticated = caller.AuthenticationState as Authenticated;
+            connectionsProvider.Users.TryAdd(authenticated!.UserId, authenticated);
             await caller.Write(new TokensPacket(packet.CorrelationId, jwtToken, refreshToken));
             return;
         }
         
-        // todo failure packet
-        // caller.Write(new AuthenticationFailurePacket());
+        await caller.Write(new WrongLoginPacket(packet.CorrelationId, "Wrong login"));
     }
 }
