569abfe0dd0c128f92494d6dc418c30daddc83c2
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 00:34:35 +0300

    add AuthValidationService

---
+++ OnChat.Server/UsersManagement/Authentication/AuthValidationService.cs
@@ -0,0 +1,19 @@
+﻿using OnChat.Encryption;
+using OnChat.Protocol.PacketHandler;
+using OnChat.Shared;
+using OnChat.Shared.Auth;
+
+namespace OnChat.UsersManagement.Authentication;
+
+public class AuthValidationService(TokensService tokensService)
+{
+    public async Task<AuthenticationState> CheckAuthentication(AuthenticatedPacket packet, IConnection connection)
+    {
+        if (tokensService.TryValidateToken(packet.Token, out _) &&
+            connection.AuthenticationState is Authenticated authenticated) 
+            return authenticated;
+        
+        await connection.Write(new UnauthorizedPacket(packet.CorrelationId, "Unauthorized"));
+        return new NotAuthenticated();
+    }
+}
